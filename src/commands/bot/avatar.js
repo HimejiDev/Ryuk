f1e03e6e842519fd24638d907bffcdb1a57c4913
dc: @himeji <tobin.post@gmail.com>
2023-11-03 21:54:43 +0100

    🚩 Added "avatar", to change the bot's avatar

---
+++ src/commands/bot/avatar.js
@@ -0,0 +1,20 @@
+const log = require("../../logger");
+const chalk = require("chalk");
+
+module.exports = {
+  name: "avatar",
+  description: "Change the bot's avatar.",
+  aliases: [],
+  usage: "avatar <url>",
+  run: async function (client, args) {
+    const url = args.join(" ");
+    if (!url)
+      return log.success(
+        `Current avatar: ${chalk.cyan(client.user.avatarURL())}`
+      );
+    await client.user
+      .setAvatar(url)
+      .then((user) => log.success(`Changed avatar to ${user.avatarURL()}`))
+      .catch((err) => log.error(err, "src/commands/bot/avatar.js"));
+  },
+};
