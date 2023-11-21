fe720046e55023d0d1e04e66681874bc244b7e50
dc: @himeji <tobin.post@gmail.com>
2023-11-21 21:53:54 +0100

    ✨ Added option for multiple members, ban command

--- src/commands/member/ban.js
+++ src/commands/member/ban.js
@@ -3,36 +3,46 @@ const chalk = require("chalk");
 
 module.exports = {
   name: "ban",
-  description: "Bans a member from the guild.",
+  description: "Bans member(s) from the guild.",
   aliases: [],
-  usage: "ban <userid> [reason]",
+  usage: "ban <userids/all>",
+  flags: { "--reason": "The reason for the ban." },
   run: async function (client, args) {
     const guild = client.target_guild;
     if (args.length < 1) {
-      log.error("You must provide a member ID");
+      log.error("You must provide at least one member ID");
       return;
     }
 
-    const id = args[0];
-    const member = await guild.members.fetch(id);
-    if (!member) {
-      log.error("Member not found");
-      return;
+    const ids = args[0].split(",");
+    const reasonIndex = args.findIndex(
+      (arg) => arg.toLowerCase() === "--reason"
+    );
+    let reason = "";
+
+    if (reasonIndex !== -1) {
+      reason = args.slice(reasonIndex + 1).join(" ");
     }
 
-    const reason = args.slice(1).join(" ");
+    for (const id of ids) {
+      const member = await guild.members.fetch(id.trim()).catch(() => null);
+      if (!member) {
+        log.error(`Member with ID ${id.trim()} not found`);
+        continue;
+      }
 
-    await member
-      .ban({ reason: reason })
-      .then((banned) =>
-        log.success(
-          `${chalk.gray("[")}${chalk.red("-")}${chalk.gray(
-            "]"
-          )} Banned ${chalk.white(banned.user.tag)} from ${chalk.white(
-            guild.name
-          )}`
+      await member
+        .ban({ reason: reason })
+        .then((banned) =>
+          log.success(
+            `${chalk.gray("[")}${chalk.red("-")}${chalk.gray(
+              "]"
+            )} Banned ${chalk.white(banned.user.tag)} from ${chalk.white(
+              guild.name
+            )}`
+          )
         )
-      )
-      .catch((err) => log.error(err, "src/commands/member/ban.js"));
+        .catch((err) => log.error(err, "src/commands/member/ban.js"));
+    }
   },
 };
