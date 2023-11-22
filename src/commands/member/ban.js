6b7c9b825ac65c7434e5174043e217c3867a688b
dc: @himeji <tobin.post@gmail.com>
2023-11-22 20:32:20 +0100

    ✨ Logging, webhook, when banning member(s)

--- src/commands/member/ban.js
+++ src/commands/member/ban.js
@@ -1,3 +1,4 @@
+const { EmbedBuilder } = require("discord.js");
 const log = require("../../logger");
 const chalk = require("chalk");
 
@@ -5,8 +6,8 @@ module.exports = {
   name: "ban",
   description: "Bans member(s) from the guild.",
   aliases: [],
-  usage: "ban <userids/all>",
-  flags: { "--reason": "The reason for the ban." },
+  usage: "ban <userids/all> [-r reason]",
+  flags: { "-r": "The reason for the ban." },
   run: async function (client, args) {
     const guild = client.target_guild;
     if (args.length < 1) {
@@ -14,35 +15,59 @@ module.exports = {
       return;
     }
 
-    const ids = args[0].split(",");
-    const reasonIndex = args.findIndex(
-      (arg) => arg.toLowerCase() === "--reason"
-    );
-    let reason = "";
+    const reasonIndex = args.findIndex((arg) => arg.toLowerCase() === "-r");
+    const ids = reasonIndex === -1 ? args : args.slice(0, reasonIndex);
+    let reason =
+      reasonIndex !== -1 ? args.slice(reasonIndex + 1).join(" ") : "";
 
-    if (reasonIndex !== -1) {
-      reason = args.slice(reasonIndex + 1).join(" ");
-    }
+    reason = reason ? `${reason} ; 🧨` : "🧨";
+
+    var bans = 0;
+    await client.webhook.send({
+      username: client.user.tag,
+      avatarURL: client.user.avatarURL(),
+      content: `\`\`\`ansi\n🧨 Banning members...\n\`\`\``,
+    });
 
     for (const id of ids) {
-      const member = await guild.members.fetch(id.trim()).catch(() => null);
-      if (!member) {
-        log.error(`Member with ID ${id.trim()} not found`);
-        continue;
-      }
+      try {
+        const member = await guild.members.fetch(id.trim());
+        //await member.ban({ reason: reason });
 
-      await member
-        .ban({ reason: reason })
-        .then((banned) =>
-          log.success(
-            `${chalk.gray("[")}${chalk.red("-")}${chalk.gray(
-              "]"
-            )} Banned ${chalk.white(banned.user.tag)} from ${chalk.white(
-              guild.name
-            )}`
-          )
-        )
-        .catch((err) => log.error(err, "src/commands/member/ban.js"));
+        // Log success message
+        const reasonLog =
+          reason !== "🧨"
+            ? ` with reason ${chalk.white(reason.replace(" ; 🧨", ""))}`
+            : "";
+        log.success(
+          `${chalk.gray("[")}${chalk.red("-")}${chalk.gray(
+            "]"
+          )} Banned ${chalk.white("@" + member.user.tag)} from ${chalk.white(
+            guild.name
+          )}${reasonLog}`
+        );
+        // edit webhook message
+        bans++;
+        await client.webhook.send({
+          username: client.user.tag,
+          avatarURL: client.user.avatarURL(),
+          content: `\`\`\`ansi\n[[2;31m-[0m][0;2m Banned [0;37m@${member.user.tag}[0m from [0;37m${
+            guild.name
+          }[0m ${
+            reason !== "🧨" ? " with reason [0;37m" + reason.replace(" ; 🧨", "") : ""
+          }[0m. [2;37m${bans}[0m/[2;37m${ids.length}[0m[0m\n\`\`\``,
+        });
+      } catch (err) {
+        log.error(
+          `Failed to ban member with ID ${id.trim()}: ${err}`,
+          "src/commands/member/ban.js"
+        );
+      }
     }
+    await client.webhook.send({
+      username: client.user.tag,
+      avatarURL: client.user.avatarURL(),
+      content: `\`\`\`ansi\n🧨 Banned members! [2;37m${bans}[0m/[2;37m${ids.length}[0m\n\`\`\``,
+    });
   },
 };
