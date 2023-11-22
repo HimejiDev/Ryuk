4d6906b30de888ce42d35bb2f83013ca6707ce22
dc: @himeji <tobin.post@gmail.com>
2023-11-22 21:00:49 +0100

    ✨ Added multiple unbanning and logging.

--- src/commands/member/unban.js
+++ src/commands/member/unban.js
@@ -5,7 +5,8 @@ module.exports = {
   name: "unban",
   description: "Unbans a member from the guild.",
   aliases: [],
-  usage: "unban <userid>",
+  usage: "unban <userids/all>",
+  flags: {},
   run: async function (client, args) {
     const guild = client.target_guild;
     if (args.length < 1) {
@@ -13,16 +14,45 @@ module.exports = {
       return;
     }
 
-    const id = args[0];
-    await guild.members
-      .unban(id)
-      .then((user) =>
+    var ids = args;
+    if (ids[0].toLowerCase() === "all") {
+      ids = await guild.bans.fetch();
+      ids = ids.map((member) => member.user.id);
+    } // [2;32m+[0m
+
+    var unbans = 0;
+    await client.webhook.send({
+      username: client.user.tag,
+      avatarURL: client.user.avatarURL(),
+      content: `\`\`\`ansi\n🧨 Unbanning members...\n\`\`\``,
+    });
+
+    for (const id of ids) {
+      try {
+        const member = await guild.members.unban(id.trim());
         log.success(
           `${chalk.gray("[")}${chalk.green("+")}${chalk.gray(
             "]"
-          )} Unbanned ${chalk.white(user.tag)} from ${chalk.white(guild.name)}`
-        )
-      )
-      .catch((err) => log.error(err, "src/commands/member/ban.js"));
+          )} Unbanned ${chalk.white(member.tag)} from ${chalk.white(
+            guild.name
+          )}`
+        );
+
+        unbans++;
+        await client.webhook.send({
+          username: client.user.tag,
+          avatarURL: client.user.avatarURL(),
+          content: `\`\`\`ansi\n[[2;32m+[0m][0;2m Unbanned [0;37m@${member.tag}[0m from [0;37m${guild.name}[0m [2;37m${unbans}[0m/[2;37m${ids.length}[0m[0m\n\`\`\``,
+        });
+      } catch (err) {
+        log.error(err, "src/commands/member/unban.js");
+      }
+    }
+
+    await client.webhook.send({
+      username: client.user.tag,
+      avatarURL: client.user.avatarURL(),
+      content: `\`\`\`ansi\n🧨 Unbanned members! [2;37m${unbans}[0m/[2;37m${ids.length}[0m\n\`\`\``,
+    });
   },
 };
