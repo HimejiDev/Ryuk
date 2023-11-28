70b87ddcb43819d28aad2e9bf16ad825f4d03f46
dc: @himeji <tobin.post@gmail.com>
2023-11-28 22:41:16 +0100

    🚸 Removed webhook logging before and during.

--- src/commands/member/ban.js
+++ src/commands/member/ban.js
@@ -15,6 +15,7 @@ module.exports = {
       return;
     }
 
+    var bans = 0;
     const reasonIndex = args.findIndex((arg) => arg.toLowerCase() === "-r");
     var ids = reasonIndex === -1 ? args : args.slice(0, reasonIndex);
     if (ids[0].toLowerCase() === "all") {
@@ -22,22 +23,13 @@ module.exports = {
     }
     let reason =
       reasonIndex !== -1 ? args.slice(reasonIndex + 1).join(" ") : "";
-
     reason = reason ? `${reason} ; 🧨` : "🧨";
-
-    var bans = 0;
-    await client.webhook.send({
-      username: client.user.tag,
-      avatarURL: client.user.avatarURL(),
-      content: `\`\`\`ansi\n🧨 Banning members...\n\`\`\``,
-    });
 
     for (const id of ids) {
       try {
         const member = await guild.members.fetch(id.trim());
         await member.ban({ reason: reason });
 
-        // Log success message
         const reasonLog =
           reason !== "🧨"
             ? ` with reason ${chalk.white(reason.replace(" ; 🧨", ""))}`
@@ -49,17 +41,8 @@ module.exports = {
             guild.name
           )}${reasonLog}`
         );
-        // edit webhook message
+
         bans++;
-        await client.webhook.send({
-          username: client.user.tag,
-          avatarURL: client.user.avatarURL(),
-          content: `\`\`\`ansi\n[[2;31m-[0m][0;2m Banned [0;37m@${member.user.tag}[0m from [0;37m${
-            guild.name
-          }[0m ${
-            reason !== "🧨" ? " with reason [0;37m" + reason.replace(" ; 🧨", "") : ""
-          }[0m. [2;37m${bans}[0m/[2;37m${ids.length}[0m[0m\n\`\`\``,
-        });
       } catch (err) {
         log.error(
           `Failed to ban member with ID ${id.trim()}: ${err}`,
--- src/commands/member/unban.js
+++ src/commands/member/unban.js
@@ -14,18 +14,12 @@ module.exports = {
       return;
     }
 
+    var unbans = 0;
     var ids = args;
     if (ids[0].toLowerCase() === "all") {
       ids = await guild.bans.fetch();
       ids = ids.map((member) => member.user.id);
-    } // [2;32m+[0m
-
-    var unbans = 0;
-    await client.webhook.send({
-      username: client.user.tag,
-      avatarURL: client.user.avatarURL(),
-      content: `\`\`\`ansi\n🧨 Unbanning members...\n\`\`\``,
-    });
+    }
 
     for (const id of ids) {
       try {
@@ -39,11 +33,6 @@ module.exports = {
         );
 
         unbans++;
-        await client.webhook.send({
-          username: client.user.tag,
-          avatarURL: client.user.avatarURL(),
-          content: `\`\`\`ansi\n[[2;32m+[0m][0;2m Unbanned [0;37m@${member.tag}[0m from [0;37m${guild.name}[0m [2;37m${unbans}[0m/[2;37m${ids.length}[0m[0m\n\`\`\``,
-        });
       } catch (err) {
         log.error(err, "src/commands/member/unban.js");
       }
