e89f56c4aa5ad44c6e1bb0350b78830c294882da
dc: @himeji <tobin.post@gmail.com>
2023-11-29 17:01:33 +0100

    ✨ Added reload command, to reload guild (info)
    Because sometimes, it would not get the new members.

---
+++ src/commands/general/reload.js
@@ -0,0 +1,23 @@
+const log = require("../../logger");
+const chalk = require("chalk");
+
+module.exports = {
+  name: "reload",
+  description: "Reloads the guild.",
+  aliases: [],
+  usage: "reload",
+  run: async function (client, args) {
+    try {
+      const GUILD_ID = client.INFO[1] || client.target_guild.id;
+      client.target_guild = client.guilds.cache.get(GUILD_ID);
+
+      log.success(`Reloaded guild ${chalk.white(client.target_guild.name)}`);
+    } catch (err) {
+      log.error(
+        `Failed to reload guild: ${err}`,
+        "src/commands/general/reload.js"
+      );
+      return;
+    }
+  },
+};
