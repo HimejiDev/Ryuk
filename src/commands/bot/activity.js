617e8e03199ad2bb50bd3de4939967dea47cb828
dc: @himeji <tobin.post@gmail.com>
2023-12-08 23:48:02 +0100

    ✨ Added activity command

---
+++ src/commands/bot/activity.js
@@ -0,0 +1,50 @@
+const log = require("../../logger");
+const chalk = require("chalk");
+const { ActivityType } = require("discord.js");
+
+module.exports = {
+  name: "activity",
+  description: "Shows and changes the bot's activity.",
+  aliases: [],
+  usage: "activity [type] [state]",
+  flags: {},
+  run: async function (client, args) {
+    if (args.length > 1) {
+      var activity = ActivityType.Custom;
+      if (args[0].toLowerCase() == "playing") {
+        activity = ActivityType.Playing;
+      } else if (args[0].toLowerCase() == "streaming") {
+        activity = ActivityType.Streaming;
+      } else if (args[0].toLowerCase() == "listening") {
+        activity = ActivityType.Listening;
+      } else if (args[0].toLowerCase() == "watching") {
+        activity = ActivityType.Watching;
+      } else if (args[0].toLowerCase() == "competing") {
+        activity = ActivityType.Competing;
+      }
+
+      await client.user.setActivity({
+        type: activity,
+        name:
+          activity !== ActivityType.Custom
+            ? args.slice(1).join(" ")
+            : "customstatus",
+        state: args.slice(1).join(" "),
+      });
+      log.success(
+        `Changed activy: ${chalk.cyan(
+          client.user.presence.activities[0].state
+        )}`
+      );
+      log.debug(
+        `Activity changes sometimes have trouble updating. It appears to be a visual bug on the client.`
+      );
+    } else {
+      log.success(
+        `Current activity: ${chalk.cyan(
+          client.user.presence.activities[0].state
+        )}`
+      );
+    }
+  },
+};
