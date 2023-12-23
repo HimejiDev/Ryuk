02a7f9fde7b5b4e82d104437a948e9e64a766733
dc: @himeji <tobin.post@gmail.com>
2023-12-24 00:37:31 +0100

    💄 Added command usage and categories to table.

--- src/commands/general/help.js
+++ src/commands/general/help.js
@@ -14,14 +14,13 @@ module.exports = {
     const category = args[0] ? args[0] : "general";
 
     if (folders.includes(category)) {
-      var table = new AsciiTable(/*`${capitalize(category)} Commands`*/);
+      var table = new AsciiTable(`${capitalize(category)} Commands`);
       table
         .setHeading("Name", "Description", "Usage", "Flags", "Aliases")
         .setBorder("|", "-", "+", "+");
 
       fs.readdirSync(`./src/commands/${category}`).forEach((cmd) => {
         const command = require(`../${category}/${cmd}`);
-        // format flags: [flag]: [description] (e.g. -r: The reason for the ban.) devide by comma
         table.addRow(
           command.name,
           command.description,
@@ -41,13 +40,27 @@ module.exports = {
         .forEach((line) => {
           log.info(line);
         });
+
+      var lastRow = table.toString().split("\n").slice(-1)[0];
+      var commandLength = "| <command> <arguments> <flags> |".length;
+      var spacesNeeded = lastRow.length - commandLength;
+      var spaces = " ".repeat(spacesNeeded > 0 ? spacesNeeded : 0);
+
+      log.info(`| <command> <arguments> <flags> ${spaces}|`);
+      log.info(table.toString().split("\n").slice(-1)[0]);
     } else {
       log.error("Invalid category");
     }
 
+    commandLength = `| Categories: ${folders
+      .map((folder) => folder)
+      .join(", ")} |`.length;
+    spacesNeeded = lastRow.length - commandLength;
+    spaces = " ".repeat(spacesNeeded > 0 ? spacesNeeded : 0);
     log.info(
-      `Categories: ${folders.map((folder) => capitalize(folder)).join(", ")}`
+      `| Categories: ${folders.map((folder) => folder).join(", ")} ${spaces}|`
     );
+    log.info(table.toString().split("\n").slice(-1)[0]);
   },
 };
 
