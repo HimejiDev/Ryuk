7be6833783d575efc7ea148379981eea549530d5
dc: @himeji <tobin.post@gmail.com>
2023-12-27 18:27:09 +0100

    ✨ Added command to change/show banner
    Not tested, because it requires boost lvl 2

---
+++ src/commands/guild/banner.js
@@ -0,0 +1,24 @@
+const log = require("../../logger");
+
+module.exports = {
+  name: "banner",
+  description: "Changes/Shows the Guild's banner.",
+  aliases: [],
+  usage: "banner [url]",
+  flags: {},
+  run: async function (client, args, flags) {
+    const guild = client.target_guild;
+
+    if (args.length > 0) {
+      const url = args.join(" ");
+      await guild
+        .setBanner(url)
+        .then((updated) =>
+          log.success(`Changed guild banner: ${updated.bannerURL()}`)
+        )
+        .catch((err) => log.error(err));
+    } else {
+      log.success(`Current guild banner: ${guild.bannerURL()}`);
+    }
+  },
+};
