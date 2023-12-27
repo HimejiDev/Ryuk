f93cba49d224c5b53afab4352fec16d3d5f5d533
dc: @himeji <tobin.post@gmail.com>
2023-12-27 18:26:22 +0100

    ✨ Added command to change/show discovery splash

---
+++ src/commands/guild/discoverysplash.js
@@ -0,0 +1,28 @@
+const log = require("../../logger");
+
+module.exports = {
+  name: "discoverysplash",
+  description: "Changes/Shows the Guild's discovery splash.",
+  aliases: ["ds", "dsplash"],
+  usage: "discoverysplash [url]",
+  flags: {},
+  run: async function (client, args, flags) {
+    const guild = client.target_guild;
+
+    if (args.length > 0) {
+      const url = args.join(" ");
+      await guild
+        .setDiscoverySplash(url)
+        .then((updated) =>
+          log.success(
+            `Changed guild discovery splash: ${updated.discoverySplashURL()}`
+          )
+        )
+        .catch((err) => log.error(err));
+    } else {
+      log.success(
+        `Current guild discovery splash: ${guild.discoverySplashURL()}`
+      );
+    }
+  },
+};
