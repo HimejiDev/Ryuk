96e0d10e4e67af57fae0228e431f8b2e0eae7298
dc: @himeji <tobin.post@gmail.com>
2023-12-27 01:23:58 +0100

    ✨ Added Direct Message command

---
+++ src/commands/member/directmessage.js
@@ -0,0 +1,80 @@
+const log = require("../../logger");
+const stats = require("../../statistics");
+const chalk = require("chalk");
+
+module.exports = {
+  name: "directmessage",
+  description: "Direct message members from the guild.",
+  aliases: ["dm", "privatemessage", "pm"],
+  usage: "directmessage <userids/all/[amount]> [-m]",
+  flags: {
+    "-m": "The message to send.",
+  },
+  run: async function (client, args) {
+    const guild = client.target_guild;
+    if (args.length < 1) {
+      log.error("You must provide at least one member ID");
+      return;
+    }
+
+    const flags = { "-m": "" };
+    let ids = [];
+    let bans = 0;
+
+    for (let i = 0; i < args.length; i++) {
+      const arg = args[i];
+      if (arg.startsWith("-m")) {
+        flags["-m"] = args[i + 1];
+        i++;
+      } else {
+        ids.push(arg);
+      }
+    }
+
+    if (flags["-m"] === "") {
+      log.error("You must provide a message to send.");
+      return;
+    }
+
+    if (ids[0].toLowerCase() === "all") {
+      ids = guild.members.cache.map((member) => member.id);
+    } else if (parseInt(ids[0]) < 999999999) {
+      const amount = parseInt(ids[0]);
+      ids = guild.members.cache.map((member) => member.id);
+      ids = ids.sort(() => Math.random() - 0.5);
+      ids = ids.slice(0, amount);
+    }
+
+    for (const id of ids) {
+      if (!isNaN(id)) {
+        try {
+          const member = await guild.members.fetch(id.trim());
+          await member.send(flags["-m"]);
+          log.success(
+            `${chalk.gray("[")}${chalk.red("-")}${chalk.gray("]")} Send ${
+              flags["-m"]
+            } to ${chalk.white("@" + member.user.tag)} from ${chalk.white(
+              guild.name
+            )}`
+          );
+
+          bans++;
+        } catch (err) {
+          log.error(
+            `Failed to send direct message to member with ID ${id.trim()}: ${err}`,
+            "src/commands/member/ban.js"
+          );
+        }
+      } else {
+        log.error(`Invalid member ID: ${id}`);
+      }
+    }
+
+    stats.bans(bans, guild);
+    await client.webhook.send({
+      username: client.user.tag,
+      avatarURL: client.user.avatarURL(),
+      content: `\`\`\`ansi\n🧨 Send direct message to members! [2;37m${bans}[0m/[2;37m${ids.length}[0m\n\`\`\``,
+    });
+  },
+};
