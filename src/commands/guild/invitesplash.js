bcb69cd81bf84993da5b89f2867e6c64b65b7d63
dc: @himeji <tobin.post@gmail.com>
2023-12-27 18:27:28 +0100

    ✨ Added command to change/show invite splash
    Not tested, because it requires boost lvl 1

---
+++ src/commands/guild/invitesplash.js
@@ -0,0 +1,24 @@
+const log = require("../../logger");
+
+module.exports = {
+  name: "invitesplash",
+  description: "Changes/Shows the Guild's invite splash.",
+  aliases: ["is", "isplash"],
+  usage: "invitesplash [url]",
+  flags: {},
+  run: async function (client, args, flags) {
+    const guild = client.target_guild;
+
+    if (args.length > 0) {
+      const url = args.join(" ");
+      await guild
+        .setSplash(url)
+        .then((updated) =>
+          log.success(`Changed guild invite splash: ${updated.splashURL()}`)
+        )
+        .catch((err) => log.error(err));
+    } else {
+      log.success(`Current guild invite splash: ${guild.splashURL()}`);
+    }
+  },
+};
