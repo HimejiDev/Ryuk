852fe71f1f9144ea5b00e25532eee924518c7703
dc: @himeji <tobin.post@gmail.com>
2023-12-30 17:24:24 +0100

    ✨ Added rules channel command

---
+++ src/commands/guild/ruleschannel.js
@@ -0,0 +1,32 @@
+const log = require("../../logger");
+const chalk = require("chalk");
+
+module.exports = {
+  name: "ruleschannel",
+  description: "Controls the rules channel.",
+  aliases: ["rc", "rules"],
+  usage: "ruleschannel [<channelid>|none]",
+  flags: {},
+  run: async function (client, args, flags) {
+    const guild = client.target_guild;
+
+    if (args.length > 0) {
+      await guild
+        .setRulesChannel(args[0] === "none" ? null : args[0])
+        .then((updated) =>
+          log.success(
+            `Changed public updates channel: ${chalk.white(
+              updated.rulesChannel?.name || "None"
+            )}`
+          )
+        )
+        .catch((err) => log.error(err));
+    } else {
+      log.success(
+        `Current public updates channel: ${chalk.white(
+          guild.rulesChannel?.name || "None"
+        )}`
+      );
+    }
+  },
+};
