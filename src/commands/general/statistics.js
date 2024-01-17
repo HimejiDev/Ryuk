e6b335dbb8a975f8a2bf4bc6779fbc5341e5a322
dc: @himeji <tobin.post@gmail.com>
2024-01-17 23:40:50 +0100

    ✨ [UPDATE] Updated "stats" command with more stats

--- src/commands/general/statistics.js
+++ src/commands/general/statistics.js
@@ -19,13 +19,22 @@ module.exports = {
       table.setTitle(`${guild.name} Statistics`);
       table.setHeading("Type", "Value");
       table.addRow("ID", guild.id);
-      table.addRow("Messages", guild.actionsmessages);
-      table.addRow("Bans", guild.actions.bans);
-      table.addRow("Kicks", guild.actions.kicks);
-      table.addRow("Mutes", guild.actions.mutes);
-      table.addRow("Nukes", guild.actions.nukes);
-      table.addRow("Current Memebers", guild.members);
-      table.addRow("Max Memebers", guild.max.members);
+      table.addRow("Members  // Bans", guild.actions.bans);
+      table.addRow("Members  // Kicks", guild.actions.kicks);
+      table.addRow("Members  // Mutes", guild.actions.mutes);
+      table.addRow("Members  // DMs", guild.actions.direct_messages);
+      table.addRow("Channels // Messages", guild.actions.messages);
+      table.addRow("Channels // Created", guild.actions.channels_create);
+      table.addRow("Channels // Deleted", guild.actions.channels_delete);
+      table.addRow("Roles    // Created", guild.actions.roles_create);
+      table.addRow("Roles    // Deleted", guild.actions.roles_delete);
+      table.addRow("Emojis   // Created", guild.actions.emojis_create);
+      table.addRow("Emojis   // Deleted", guild.actions.emojis_delete);
+      table.addRow("Stickers // Created", guild.actions.stickers_create);
+      table.addRow("Stickers // Deleted", guild.actions.stickers_delete);
+      table.addRow("Guild    // Nukes", guild.actions.nukes);
+      table.addRow("Guild    // Current Memebers", guild.members);
+      table.addRow("Guild    // Max Memebers", guild.max.members);
     } else if (guild_id === "guilds") {
       table.setHeading("ID", "Name", "Members");
       for (const guild of stats.guilds()) {
@@ -34,12 +43,21 @@ module.exports = {
     } else {
       table.setTitle(`Statistics`);
       table.setHeading("Type", "Value");
-      table.addRow("Guilds", stats.guilds().length);
-      table.addRow("Messages", stats.get("messages"));
-      table.addRow("Bans", stats.get("bans"));
-      table.addRow("Kicks", stats.get("kicks"));
-      table.addRow("Mutes", stats.get("mutes"));
-      table.addRow("Nukes", stats.get("nukes"));
+      table.addRow("Bot      // Guilds", stats.guilds().length);
+      table.addRow("Users    // Bans", stats.get("bans"));
+      table.addRow("Users    // Kicks", stats.get("kicks"));
+      table.addRow("Users    // Mutes", stats.get("mutes"));
+      table.addRow("Users    // DMs", stats.get("direct_messages"));
+      table.addRow("Channels // Messages", stats.get("messages"));
+      table.addRow("Channels // Created", stats.get("channels_create"));
+      table.addRow("Channels // Deleted", stats.get("channels_delete"));
+      table.addRow("Roles    // Created", stats.get("channels_create"));
+      table.addRow("Roles    // Deleted", stats.get("channels_delete"));
+      table.addRow("Emojis   // Created", stats.get("channels_create"));
+      table.addRow("Emojis   // Deleted", stats.get("channels_delete"));
+      table.addRow("Stickers // Created", stats.get("channels_create"));
+      table.addRow("Stickers // Deleted", stats.get("channels_delete"));
+      table.addRow("Guilds   // Nukes", stats.get("nukes"));
     }
 
     await client.webhook.send({
